e2198d4f3fad385d1a5fe5e1027b97f815329644
Denix7 <dennis.brian.pv@gmail.com>
2020-07-08 18:41:11 -0400

    Send rentals, parent to child component

--- src/app/components/rental/rental-item/rental-item.component.ts
+++ src/app/components/rental/rental-item/rental-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rental-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentalItemComponent implements OnInit {
 
+  @Input() rental:any;
   constructor() { }
 
   ngOnInit(): void {
