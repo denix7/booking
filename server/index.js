8b63037f89cb048483c5affb47e3cb544e72622f
Denix7 <dennis.brian.pv@gmail.com>
2020-07-11 18:28:03 -0400

    DB conect

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+
+mongoose.connect(config.database, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    if(err)
+        console.log(err);
+    else 
+        console.log('Db connected');
+});
 
 const app = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || config.port;
 
 app.listen(PORT, ()=> {
-    console.log('server on');
+    console.log('server listen on port ', PORT);
 });
 
 app.get('/rentals', (req, res) => {
