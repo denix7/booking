a849af4a1eb06a369e805c2df421e3d6d9653bcf
Denix7 <dennis.brian.pv@gmail.com>
2020-07-11 13:22:03 -0400

    router link for rental detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ const routes : Routes = [
         redirectTo: 'rentals', 
         pathMatch: 'full'
     },
-    {
-        path : '**', 
-        redirectTo: 'rentals', 
-        pathMatch: 'full'
-    },
+    // {
+    //     path : '**', 
+    //     redirectTo: 'rentals', 
+    //     pathMatch: 'full'
+    // },
 ];
 
 @NgModule({
--- src/app/components/rental/rental-detail/rental-detail.component.ts
+++ src/app/components/rental/rental-detail/rental-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-rental-detail',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-detail.component.css']
 })
 export class RentalDetailComponent implements OnInit {
-
-  constructor() { }
+  myParam;
+  constructor(private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.myParam = params.get('rentalId');
+    })
   }
 
 }
