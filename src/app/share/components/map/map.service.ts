90ed4440ebf39fca3d091b05a23b7d2715c47120
Denix7 <dennis.brian.pv@gmail.com>
2020-07-22 21:28:50 -0400

    Caching the location for reduce petitions

--- src/app/share/components/map/map.service.ts
+++ src/app/share/components/map/map.service.ts
@@ -12,27 +12,41 @@ export class MapService {
 
   constructor(private camelizePipe:CamelizePipe) { }
 
+  private camelize(location : string) : string{
+    return this.camelizePipe.transform(location);
+  }
+
+  public isLocationCached(location : string) : boolean{
+    return this.locationCache[this.camelize(location)];
+  }
+
   public cacheLocation(location:string, coordinates:any){
-    const camelizedLocation = this.camelizePipe.transform(location);
+    const camelizedLocation = this.camelize(location);
+    // console.warn('location saved from service to JSON!');
     this.locationCache[camelizedLocation] = coordinates;
   }
 
   public geoCodeLocation(location:string): Observable<any>{
     this.geoCoder = new (<any>window).google.maps.Geocoder();
-    
     return new Observable((observer)=> {
-      this.geoCoder.geocode({address: location}, (result, status) => {
-        if(status === 'OK'){
-          const geometry = result[0].geometry.location;
-
-          const coordinates = {lat: geometry.lat(), lng: geometry.lng()};
-          this.cacheLocation(location, coordinates);
-
-          observer.next(coordinates);
-        }else{
-          observer.error('Location could not be geocoded');
-        }
-      });
+      if(this.isLocationCached(location)){
+        observer.next(this.locationCache[this.camelize(location)]);
+        // console.warn('location from cache!');
+      }
+      else{
+        this.geoCoder.geocode({address: location}, (result, status) => {
+          if(status === 'OK'){
+            const geometry = result[0].geometry.location;
+            const coordinates = {lat: geometry.lat(), lng: geometry.lng()};
+            
+            this.cacheLocation(location, coordinates);
+            // console.warn('location from service!')
+            observer.next(coordinates);
+          }else{
+            observer.error('Location could not be geocoded');
+          }
+        });
+      }
     });
   }
 }
